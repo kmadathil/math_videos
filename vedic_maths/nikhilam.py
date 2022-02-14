b105e7de479e38d11ac76b08f22bf7875512e77a
Karthik Madathil <karthik.m@atonarp.com>
2022-02-14 12:51:36 -0700

    Updated nikhilam videos

--- vedic_maths/nikhilam.py
+++ vedic_maths/nikhilam.py
@@ -10,16 +10,20 @@ def Title(scene, t0, t1, wait=5, scale=0.3, move=(3, 6)):
     scene.add(tg)
     scene.wait(wait)
     scene.play(tg.animate.scale(scale).move_to(move[0]*DOWN+move[1]*RIGHT))
+    return tg
 
 # Helper function for multi-line explanation
-def Explanation(scene, text, wait=3):
+def Explanation(scene, text, wait=3, fade=True):
     # Explanation
     el = [MarkupText(x) for x in text]
     eg = VGroup(*el).scale(0.7).arrange(DOWN)
     for _el in el:
         scene.play(AddTextLetterByLetter(_el, time_per_letter=1))
-    scene.wait(wait)
-    scene.play(FadeOut(eg))
+    if wait:
+        scene.wait(wait)
+    if fade:
+        scene.play(FadeOut(eg))
+    return eg
 
 # Helper function to display text message
 def DisplayText(scene, text, scale=1, move=(0, 0), wait=5, fade=True):
@@ -28,7 +32,94 @@ def DisplayText(scene, text, scale=1, move=(0, 0), wait=5, fade=True):
     scene.wait(wait)
     if fade:
         scene.play(FadeOut(t))
+    return t
 
+
+# Helper function for Sutra display
+# Inputs
+#     sutra     - Sutra string
+#     viccheda  - List of sandhi split segments
+#   translation - List of translation segments, matching viccheda
+def Sutra(scene, sutra, viccheda, translation, wait=3, scale=0.5, move=(3, 5), fade=False):
+    sg = VGroup(Text("सूत्रम्"), Text("विच्छेदः"), Text("Translation")).arrange(direction=DOWN, aligned_edge=RIGHT).set_color(BLUE).set_opacity(0.5)
+    #t0 = Text("निखिलं नवतश्चरमं दशतः")
+    #t1 = [Text("निखिलं नवतः "), Text("चरमं दशतः")]
+    #t2 = [Text("All from 9, "), Text("Last from 10")]
+    t0 = MarkupText(sutra)
+    t1 = [MarkupText(x) for x in viccheda]
+    t2 = [MarkupText(x) for x in translation]
+    assert len(viccheda) == len(translation), "Sandhi split (viccheda) and translation must be of equal length"
+    t1g = VGroup(*t1).arrange(direction=RIGHT).set_opacity(0)
+    t2g = VGroup(*t2).arrange(direction=RIGHT).set_opacity(0)
+    tg = VGroup(t0, t1g, t2g).arrange(direction=DOWN)
+
+    tg.move_to(UP+RIGHT)
+    sg.next_to(tg, LEFT)
+
+    scene.play(FadeIn(sg))
+    scene.play(Write(t0.set_color(ORANGE)))
+    scene.wait()
+    scene.play(t1g.animate.set_opacity(1))
+    for i in range(len(translation)):
+        scene.play(t1g[i].animate.set_color(YELLOW),
+                  t2g[i].animate.set_color(YELLOW).set_opacity(1))
+        scene.wait(5)
+
+        scene.play(t1g[i].animate.set_opacity(0.25),
+                  t2g[i].animate.set_opacity(0.25))
+    t1g.set_opacity(1)
+    t2g.set_opacity(1)
+    scene.wait(3)
+    scene.play(FadeOut(sg,t0))
+    if move is not None:
+        scene.play(t1g.set_color(WHITE).animate.scale(0.5).move_to(move[0]*UP+move[1]*RIGHT))
+        scene.play(t2g.set_color(WHITE).animate.scale(0.5).move_to((move[0]-0.5)*UP+move[1]*RIGHT))
+        scene.wait(1)
+    elif fade:
+        scene.play(FadeOut(t1g, t2g))
+    return (t1g, t2g)
+
+
+# Helper Function for Nikhilam
+def NikhilamExample(scene, num):
+    l = len(num)
+    # Text version of input
+    t2 = Text(num).set_color(YELLOW).move_to(LEFT) # Calc
+    t22 = Text(num).set_color(YELLOW) # Final Display
+
+    # Horizontal line
+    ln = Line(start=array([-1*l,0,0]), end=array([0,0,0])).set_color(YELLOW).next_to(t2, DOWN)
+
+    # COmplement
+    l9 = l - len(num.lstrip('9'))
+    p = str(10**l - int(num))
+    p0 = '0'*l9+p
+    t3 = Text(p0).set_color(ORANGE).next_to(ln, DOWN)
+    t33 = Text(p).set_color(ORANGE) #Final Display
+
+    # 9s and 10
+    # Trailing zeros
+    lz = l - len(num.rstrip('0'))
+    t = Text("9"*(l-lz-1)).scale(0.9)
+    ts = "10"
+    tt = Text(ts).scale(0.7)
+    g1 = VGroup(t,tt).arrange(direction=RIGHT, buff=0.1).set_opacity(0.8).next_to(t2,
+                                                            UP, aligned_edge=LEFT)
+
+    # Answer
+    g2 = VGroup(t33, Text("is the complement of"), t22).arrange(RIGHT).next_to(t3, 2*DOWN)
+
+    scene.play(Write(t2))
+    scene.play(FadeIn(ln))
+    scene.play(AddTextLetterByLetter(t,time_per_char=0.2))
+    scene.play(AddTextLetterByLetter(tt, time_per_char=0.2))
+    scene.play(AddTextLetterByLetter(t3,time_per_char=1))
+    scene.play(FadeIn(g2))
+
+    scene.wait(4)
+    scene.play(FadeOut(t2,g1,ln,t3, g2))
+
+        
 # Helper function for subtraction example
 def SubExample(scene, num1, num2):
         op = Text("-")
@@ -122,23 +213,14 @@ class Nikhilam(Scene):
     def construct(self):
 
         # Title Scene
-        # t0 = Text("परिपूरकम्")
-        # t1 = Text("Complement")
-        # tg = VGroup(t0,t1).arrange(direction=DOWN)
-        # self.add(tg)
-        # self.wait(5)
-        # self.play(tg.animate.scale(0.3).move_to(DOWN*3+RIGHT*6))
         Title(self, "परिपूरकम्", "Complement")
         self.next_section()
 
         # Definition
-        el = [MarkupText(x) for x in ["A Complement Completes a Number",
-                                "A <span foreground='yellow'>Number</span> and its <span foreground='orange'>Complement</span>",
-                                "always add up to a power of 10"]]
-        eg = VGroup(*el).scale(0.7).arrange(DOWN).move_to(LEFT)
-        for _el in el:
-            self.play(AddTextLetterByLetter(_el, time_per_letter=1))
-        self.wait(3)
+        el = ["A Complement Completes a Number",
+              "A <span foreground='yellow'>Number</span> and its <span foreground='orange'>Complement</span>",
+              "always add up to a power of 10"]
+        eg = Explanation(self, el, wait=3)
         self.play(eg.animate.move_to(UP*2.5))
         for k in [55, 90, 145, 270]:
             s = Sector(inner_radius=0, outer_radius=1, start_angle=0, angle=k * DEGREES, color=YELLOW)
@@ -153,47 +235,16 @@ class Nikhilam(Scene):
         
         
         # Sutra Scene
-        sg = VGroup(Text("सूत्रम्"), Text("विच्छेदः"), Text("Translation")).arrange(direction=DOWN, aligned_edge=RIGHT).set_color(BLUE).set_opacity(0.5)
-        t0 = Text("निखिलं नवतश्चरमं दशतः")
-        t1 = [Text("निखिलं नवतः "), Text("चरमं दशतः")]
-        t2 = [Text("All from 9, "), Text("Last from 10")]
-        t1g = VGroup(*t1).arrange(direction=RIGHT).set_opacity(0)
-        t2g = VGroup(*t2).arrange(direction=RIGHT).set_opacity(0)
-        tg = VGroup(t0, t1g, t2g).arrange(direction=DOWN)
-
-        tg.move_to(UP+RIGHT)
-        sg.next_to(tg, LEFT)
-        
-        self.play(FadeIn(sg))
-        self.play(Write(t0.set_color(ORANGE)))
-        self.wait()
-        self.play(t1g.animate.set_opacity(1))
-        self.play(t1g[0].animate.set_color(YELLOW),
-                  t2g[0].animate.set_color(YELLOW).set_opacity(1))
-        self.wait(5)
-
-        self.play(t1g[0].animate.set_opacity(0.25),
-                  t2g[0].animate.set_opacity(0.25))
-        self.play(t1g[1].animate.set_color(YELLOW),
-                  t2g[1].animate.set_color(YELLOW).set_opacity(1))
-        self.wait(5)
-        t1g.set_opacity(1)
-        t2g.set_opacity(1)
-        self.wait(3)
-        self.play(FadeOut(sg,t0))
-        self.play(t1g.set_color(WHITE).animate.scale(0.5).move_to(RIGHT*5+UP*3))
-        self.play(t2g.set_color(WHITE).animate.scale(0.5).move_to(RIGHT*5+UP*2.5))
-        self.wait(1)
-
+        t0 = "निखिलं नवतश्चरमं दशतः"
+        t1 = ["निखिलं नवतः ", "चरमं दशतः"]
+        t2 = ["All from 9, ", "Last from 10"]
+        Sutra(self, t0, t1, t2, wait=3, scale=0.5, move=(3, 5), fade=False)
         self.next_section()
+
         # Explanation
-        el = [Text(x) for x in ["To Calculate the Complement of a Number", "Subtract the last nonzero digit from 10", "And Subtract all digits to the left of it from 9"]]
-        eg = VGroup(*el).scale(0.7).arrange(DOWN).move_to(LEFT)
-        for _el in el:
-            self.play(AddTextWordByWord(_el, time_per_word=1))
-        self.wait(5)
+        el = ["To Calculate the Complement of a Number", "Subtract the last nonzero digit from 10", "And Subtract all digits to the left of it from 9"]
+        eg = Explanation(self, el, wait=5)
         self.play(eg.animate.scale(0.4).move_to(RIGHT*5))
-        
         self.next_section()
         
         # Example 1
@@ -204,51 +255,8 @@ class Nikhilam(Scene):
         self.play(titex.animate.scale(0.7).move_to(UP*3+LEFT))
     
         examples = ["17", "83", "189", "320", "765432" , "58730", "982000"]
-        
-        def _show(num):
-            l = len(num)
-            # Text version of input
-            t2 = Text(num).set_color(YELLOW).move_to(LEFT) # Calc
-            t22 = Text(num).set_color(YELLOW) # Final Display
-            
-            # Horizontal line
-            ln = Line(start=array([-1*l,0,0]), end=array([0,0,0])).set_color(YELLOW).next_to(t2, DOWN)
-
-            # COmplement
-            l9 = l - len(num.lstrip('9'))
-            p = str(10**l - int(num))
-            p0 = '0'*l9+p
-            t3 = Text(p0).set_color(ORANGE).next_to(ln, DOWN)
-            t33 = Text(p).set_color(ORANGE) #Final Display
-            
-            # 9s and 10
-            # Trailing zeros
-            lz = l - len(num.rstrip('0'))
-            t = Text("9"*(l-lz-1)).scale(0.9)
-            ts = "10"
-            #if lz > 0:
-            #    ts = ts+'0'*lz
-            tt = Text(ts).scale(0.7)
-            g1 = VGroup(t,tt).arrange(direction=RIGHT, buff=0.1).set_opacity(0.8).next_to(t2,
-                                                                    UP, aligned_edge=LEFT)
-
-            # Answer
-            g2 = VGroup(t33, Text("is the complement of"), t22).arrange(RIGHT).next_to(t3, 2*DOWN)
-            
-            self.play(Write(t2))
-            #self.play(Write(g1))
-            self.play(FadeIn(ln))
-            #self.play(Write(t3))
-            self.play(AddTextLetterByLetter(t,time_per_char=0.2))
-            self.play(AddTextLetterByLetter(tt, time_per_char=0.2))
-            self.play(AddTextLetterByLetter(t3,time_per_char=1))
-            self.play(FadeIn(g2))
-            
-            self.wait(4)
-            self.play(FadeOut(t2,g1,ln,t3, g2))
-                      
         for num in examples:
-            _show(num)
+           NikhilamExample(self, num)
         self.wait(5)
 
 class Subtraction(Scene):
@@ -261,6 +269,29 @@ class Subtraction(Scene):
                 "Subtraction without the pain of borrowing!"]
         Explanation(self, text)
         self.next_section()
+        # Revision and Example
+
+        # Explanation
+        eg = DisplayText(self, "Revision", fade=True)
+        self.next_section()
+
+        # Sutra Scene
+        t0 = "निखिलं नवतश्चरमं दशतः"
+        t1 = ["निखिलं नवतः ", "चरमं दशतः"]
+        t2 = ["All from 9, ", "Last from 10"]
+        Sutra(self, t0, t1, t2, wait=3, scale=0.5, move=None, fade=True)
+        self.next_section()
+        # Explanation
+        el = ["To Calculate the Complement of a Number", "Subtract the last nonzero digit from 10", "And Subtract all digits to the left of it from 9"]
+        eg = Explanation(self, el, wait=2, fade=True)
+        self.next_section()
+    
+        NikhilamExample(self, "6583200")
+        self.next_section()
+  
+        eg = DisplayText(self, "How do we use this for subtraction?", fade=True)
+        self.next_section()
+        
         # Explanation
         text = ["To subtract a number",
                 "Compute the complement of the subtrahend using निखिलं",
