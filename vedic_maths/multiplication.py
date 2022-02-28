7a2b67e762c43656e0ef4cdfc172f56332633faf
Karthik Madathil <karthik.m@atonarp.com>
2022-02-28 16:12:12 -0700

    ekadhikena

---
+++ vedic_maths/multiplication.py
@@ -0,0 +1,133 @@
+from manim import *
+from numpy import array
+from vedic_maths.common import *
+
+
+def EkadhikenaSquare(scene, num, wait=5, fade=True):
+    snum = str(num)
+    inum = int(num)
+    sprev = snum[:-1]
+    prev = int(sprev)
+    assert snum[-1] == "5", "Only works with numbers ending in 5"
+    text= [
+        f"Find the Square of <span color='yellow'>{snum}</span>",
+        f"Digit(s) before 5 are <span color='yellow'>{sprev}</span>",
+        f"One more than that is  <span color='yellow'>{prev+1}</span>",
+        f"Our answer is simply <span color='yellow'>{prev} times {prev+1}, suffixed with 25</span>",
+        f"which is <span color='yellow'>{prev*(prev+1)}{25}</span>"]
+    Explanation(scene, text, font='Cambria Math', aligned_edge=LEFT)
+    scene.next_section()
+    ans = inum**2
+    assert ans == prev*(prev+1)*100+25
+    snum2 = snum
+    f1 = f2 = 5
+    return EkCom(scene, snum, snum2, prev, f1, f2, ans, wait, fade)
+
+def EkCom(scene, snum, snum2, prev, f1, f2, ans, wait=5, fade=True):
+    g   = ShowOp(scene, snum, snum2, "×", "?", wait=1, fade=False)
+    g11   = ShowOp(scene, prev, "<span color='red'>"+str(prev+1)+"</span>", "×", prev*(prev+1), play=False)
+    g12   = ShowOp(scene, f1, f2, "×", f1*f2, play=False)
+    g12.move_to(RIGHT*2)
+    scene.play(Transform(g, g11))
+    scene.add(g12)
+    scene.wait(2)
+    scene.play(FadeOut(g[1]))
+    scene.play(g12.animate.next_to(g[0], RIGHT, aligned_edge=UP))
+    scene.wait(2)
+    g2  = ShowOp(scene, snum, snum2, "×", ans, play=False)
+    scene.remove(g12)
+    scene.play(Transform(g, g2))
+    scene.wait(wait)
+    if fade:
+        scene.play(FadeOut(g))
+    return g
+
+def EkadhikenaMult(scene, num, num2, wait=5, fade=True):
+    snum = str(num)
+    inum = int(num)
+    sprev = snum[:-1]
+    fin = int(snum[-1])
+    prev = int(sprev)
+    snum2 = str(num2)
+    sprev2 = snum2[:-1]
+    inum2 = int(num2)
+    fin2 = int(snum2[-1])
+
+    assert (int(snum[-1]) + int(snum2[-1])) == 10, "Only works with numbers with last digits ending in 10"
+    assert sprev == sprev2, "Only works with numbers with previous digits the same"
+
+    ans = inum*inum2
+    assert ans == prev*(prev+1)*100+fin*fin2
+
+    text= [
+        f"Find the product of <span color='yellow'>{snum} and {snum2}</span>",
+        f"Final digits are <span color='yellow'>{snum[-1]} and {snum2[-1]}</span>",
+        f"Their product is <span color='yellow'>{int(snum[-1])*int(snum2[-1])}</span>",
+        f"Pre-final Digit(s) are <span color='yellow'>{sprev}</span>",
+        f"One more than that is  <span color='yellow'>{prev+1}</span>",
+        f"As before, our answer begins with <span color='yellow'>{prev} times {prev+1}</span>",
+        f"But is now suffixed with <span color='yellow'>{int(snum[-1])*int(snum2[-1])}</span>",
+        f"which is <span color='yellow'>{inum*inum2}</span>"]
+    Explanation(scene, text, font='Cambria Math', aligned_edge=LEFT)
+    scene.next_section()
+    return EkCom(scene, snum, snum2, prev, fin, fin2, ans, wait, fade)
+
+
+
+class Ekadhikena(Scene):
+    ''' Ekadhikena Purvena '''
+    def construct(self):
+        # Title
+        Title(self, "एकाधिकेन पूर्वेण", "Squares and Multiplication with Ekadhikena",
+              move=(3, 5), wait=1)
+        self.next_section()
+
+        # Introduction
+        text = ["Simple multiplication and squares",
+                "Before we introduce a general method for multiplication",
+                "We will look at special cases which are easier",
+                "This promotes algorithmic thinking - picking the right method"]
+        Explanation(self, text, aligned_edge=LEFT)
+        self.next_section()
+
+        # Introduction
+        text = ["Our first method works for ",
+                "1. Squares of numbers ending in 5",
+                "2. Products of numbers with last digits adding to 10",
+                "and other digits identical"]
+        Explanation(self, text, aligned_edge=LEFT)
+        self.next_section()
+
+
+        # Sutra Scene
+        t0 = "एकाधिकेन पूर्वेण"
+        t1 = ["एकाधिकेन", "पूर्वेणः"]
+        t2 = ["<span size='smaller'>By one more than</span>", "<span size='smaller'>the previous</span>"]
+        Sutra(self, t0, t1, t2, wait=3, scale=0.5, move=None, fade=True, font='Cambria Math')
+        self.next_section()
+
+        EkadhikenaSquare(self, 35)
+        self.next_section()
+
+        EkadhikenaSquare(self, 85)
+        self.next_section()
+
+        # Sutra Scene
+        t0 = "अन्त्ययोर्दशकेऽपि"
+        t1 = ["अन्त्ययोः", "दशके अपि"]
+        t2 = ["<span size='smaller'>When the last</span>", "<span size='smaller'>sum to 10</span>"]
+        Sutra(self, t0, t1, t2, wait=3, scale=0.5, move=None, fade=True, font='Cambria Math')
+        self.next_section()
+
+        # Introduction
+        text = ["We can use this to multiply numbers that",
+                "1. have final digits adding up to 10",
+                "2. also have other digits identical"]
+        Explanation(self, text, aligned_edge=LEFT)
+        self.next_section()
+
+        EkadhikenaMult(self, 33, 37)
+        self.next_section()
+        
+        EkadhikenaMult(self, 72, 78)
+        self.next_section()
--- vedic_maths/nikhilam.py
+++ vedic_maths/nikhilam.py
@@ -1,88 +1,6 @@
 from manim import *
 from numpy import array
-
-
-# Helper Function for Title
-def Title(scene, t0, t1, wait=5, scale=0.3, move=(3, 6)):
-    # Title Scene
-    t0 = Text(t0)
-    t1 = Text(t1)
-    tg = VGroup(t0, t1).arrange(direction=DOWN)
-    scene.add(tg)
-    scene.wait(wait)
-    scene.play(tg.animate.scale(scale).move_to(move[0] * DOWN + move[1] * RIGHT))
-    return tg
-
-
-# Helper function for multi-line explanation
-def Explanation(scene, text, font='', wait=3, fade=True, aligned_edge=ORIGIN):
-    # Explanation
-
-    el = [MarkupText(x, font=font) for x in text]
-    eg = VGroup(*el).scale(0.7).arrange(DOWN, aligned_edge=aligned_edge)
-
-    for _el in el:
-        scene.play(AddTextLetterByLetter(_el, time_per_letter=1))
-    if wait:
-        scene.wait(wait)
-    if fade:
-        scene.play(FadeOut(eg))
-
-    return eg
-
-
-# Helper function to display text message
-def DisplayText(scene, text, font='', scale=1, move=(0, 0), wait=5, fade=True):
-    t = MarkupText(text, font=font).scale(scale)
-    scene.play(FadeIn(t.move_to(move[0] * DOWN + move[1] * RIGHT)))
-    scene.wait(wait)
-    if fade:
-        scene.play(FadeOut(t))
-    return t
-
-
-# Helper function for Sutra display
-# Inputs
-#     sutra     - Sutra string
-#     viccheda  - List of sandhi split segments
-#   translation - List of translation segments, matching viccheda
-def Sutra(scene, sutra, viccheda, translation, font='', wait=3, scale=0.5, move=(3, 5), fade=False):
-    sg = VGroup(Text("सूत्रम्"), Text("विच्छेदः"), Text("Translation")).arrange(direction=DOWN,
-                                                                                aligned_edge=RIGHT).set_color(
-        BLUE).set_opacity(0.5)
-    t0 = MarkupText(sutra)
-    t1 = [MarkupText(x) for x in viccheda]
-    t2 = [MarkupText(x, font=font) for x in translation]
-    assert len(viccheda) == len(translation), "Sandhi split (viccheda) and translation must be of equal length"
-    t1g = VGroup(*t1).arrange(direction=RIGHT).set_opacity(0)
-    t2g = VGroup(*t2).arrange(direction=RIGHT).set_opacity(0)
-    tg = VGroup(t0, t1g, t2g).arrange(direction=DOWN)
-
-    tg.move_to(UP + RIGHT)
-    sg.next_to(tg, LEFT)
-
-    scene.play(FadeIn(sg))
-    scene.play(Write(t0.set_color(ORANGE)))
-    scene.wait()
-    scene.play(t1g.animate.set_opacity(1))
-    for i in range(len(translation)):
-        scene.play(t1g[i].animate.set_color(YELLOW),
-                   t2g[i].animate.set_color(YELLOW).set_opacity(1))
-        scene.wait(5)
-
-        scene.play(t1g[i].animate.set_opacity(0.25),
-                   t2g[i].animate.set_opacity(0.25))
-    t1g.set_opacity(1)
-    t2g.set_opacity(1)
-    scene.wait(3)
-    scene.play(FadeOut(sg, t0))
-    if move is not None:
-        scene.play(t1g.set_color(WHITE).animate.scale(0.5).move_to(move[0] * UP + move[1] * RIGHT))
-        scene.play(t2g.set_color(WHITE).animate.scale(0.5).move_to((move[0] - 0.5) * UP + move[1] * RIGHT))
-        scene.wait(1)
-    elif fade:
-        scene.play(FadeOut(t1g, t2g))
-    return (t1g, t2g)
+from vedic_maths.common import *
 
 
 # Helper Function for Nikhilam
@@ -219,25 +137,6 @@ def SubExample(scene, num1, num2):
     # scene.wait(5)
     scene.remove(n1, ln, g3, op1, cmpl, ct, cmplc, res)
 
-
-def ShowOp(scene, sn1, sn2, sop, sr, move=(0, 0), wait=3, play=True, fade=True, oplen=0):
-    ''' Helper function to display a single operation '''
-    n1 = MarkupText(str(sn1))
-    n2 = MarkupText(str(sn2))
-    if oplen == 0:
-        oplen = len(str(sn1))
-    ln = Line(start=array([-1 * oplen / 2, 0, 0]), end=array([0, 0, 0])).set_color(YELLOW)
-    op = MarkupText(str(sop))
-    res = MarkupText(str(sr))
-    g1 = VGroup(n1, n2, ln, res).arrange(DOWN, aligned_edge=RIGHT)
-    g = VGroup(g1, op).arrange(RIGHT, aligned_edge=UP)
-    if play:
-        scene.play(Write(g.move_to(UP * move[0] + RIGHT * move[1])))
-        if wait:
-            scene.wait(wait)
-        if fade:
-            scene.play(FadeOut(g))
-    return g
 
 
 def ShowNikhilamOp(scene, sn1, sn2, sop, sr, move=(0, 0), wait=3, play=True, fade=True, oplen=0):
